21ac8bae272641b009517bfc7c0efedee44553fd
ahmedmohamed520 <frontenddeveloper12020@gmail.com>
2022-09-15 22:59:27 +0200

    lesson files

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+// Arrays
+var names = ["Ahmed", "mohamed", "farouk"];
+// names.push(10);
+var mixed = [1, 2, 3, 4, "Ahmed"];
+var numbers = [1, 2, 3, 4];
+// numbers.push("Ahemd");
+// Objects
+var me = {
+    name: "Ahmed",
+    age: 25
+};
+me.name = "Ammar";
+me.age = 26;
+// me.age ="27";
+// me.skills = ["football", "swimming"];
---
+++ sandbox.ts
@@ -0,0 +1,17 @@
+// Arrays
+const names = ["Ahmed", "mohamed", "farouk"];
+// names.push(10);
+const mixed = [1, 2, 3, 4, "Ahmed"];
+
+const numbers = [1, 2, 3, 4];
+// numbers.push("Ahemd");
+
+// Objects
+let me = {
+    name: "Ahmed",
+    age: 25,
+};
+me.name = "Ammar";
+me.age = 26;
+// me.age ="27";
+// me.skills = ["football", "swimming"];

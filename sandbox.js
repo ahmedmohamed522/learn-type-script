bd26bb68886dae2b8c4f6baddc331ab1de1d1c75
ahmedmohamed520 <frontenddeveloper12020@gmail.com>
2022-09-15 22:37:12 +0200

    lesson code

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+var character = "Ahmed";
+var age = 30;
+var isBlackBelt = true;
+// char = 5;
+character = "Mohamed";
+// age = 'Name';
+age = 25;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5));
---
+++ sandbox.ts
@@ -0,0 +1,14 @@
+let character = "Ahmed";
+let age = 30;
+let isBlackBelt = true;
+
+// char = 5;
+character = "Mohamed";
+
+// age = 'Name';
+age = 25;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+};
+console.log(circ(5));
